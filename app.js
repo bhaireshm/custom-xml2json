093a3901a96e15ce39303ce644b9b637e4d9f8e3
Bhairesh M <bhairesh@arithaconsulting.com>
2021-06-14 17:41:28 +0530

    Update: auto restart added. Exit codition added.

--- app.js
+++ app.js
@@ -3,51 +3,64 @@ var parseString = require("xml2js").parseString;
 const path = require("path");
 const fs = require("fs");
 
-prompt.start();
+start();
 
-prompt.get(["xmlUrl", "valueName"], function (err, result) {
-  var xmlUrl = result.xmlUrl.replace(/"/g, "");
-  var savePath = xmlUrl.split("\\");
-  const valueName = result.valueName ? result.valueName : false;
+function start() {
+  prompt.start();
 
-  var fileName = savePath[savePath.length - 1].replace(".xml", ".json");
+  prompt.get(["xmlUrl", "valueName"], function (err, result) {
+    var xmlUrl = result.xmlUrl.replace(/"/g, "");
+    if (xmlUrl == "0") process.exit(-1);
 
-  // removing last element
-  savePath.splice(savePath.length - 1, 1);
-  savePath = savePath.join("\\");
+    var savePath = xmlUrl.split("\\");
+    const valueName = result.valueName ? result.valueName : false;
 
-  var xmlData = fs.readFileSync(xmlUrl, "utf-8");
+    var fileName = savePath[savePath.length - 1].replace(".xml", ".json");
 
-  //  covert xml to json
-  parseString(xmlData, function (err, result) {
-    // extract list of data
-    var data = result.codeList.code;
+    // removing last element
+    savePath.splice(savePath.length - 1, 1);
+    savePath = savePath.join("\\");
 
-    data.forEach((d) => {
-      // remove description
-      delete d.description;
+    var xmlData = fs.readFileSync(xmlUrl, "utf-8");
 
-      d["codeListVersion"] = typeof d.name == "object" ? d.name[0] : d.name;
-      delete d.name;
+    //  covert xml to json
+    parseString(xmlData, function (err, result) {
+      // extract list of data
+      var data = result.codeList.code;
 
-      d["value"] = typeof d.value == "object" ? d.value[0] : d.value;
+      data.forEach((d) => {
+        // remove description
+        delete d.description;
 
-      if (valueName) {
-        d[valueName] = typeof d.value == "object" ? d.value[0] : d.value;
-        delete d.value;
-      }
+        d["codeListVersion"] = typeof d.name == "object" ? d.name[0] : d.name;
+        delete d.name;
+
+        d["value"] = typeof d.value == "object" ? d.value[0] : d.value;
+
+        if (valueName) {
+          d[valueName] = typeof d.value == "object" ? d.value[0] : d.value;
+          delete d.value;
+        }
+      });
+
+      // save it as json
+      fs.writeFileSync(path.join(savePath, fileName), JSON.stringify(data));
+      console.info(
+        "\nData extraction completed",
+        path.join(savePath, fileName),
+        "\n"
+      );
+
+      // List converted XML's
+      const completedModelsFileName = "completed-models.json";
+      const content = JSON.parse(
+        fs.readFileSync(completedModelsFileName, "utf-8")
+      );
+      if (!content.some((c) => c == fileName)) content.push(fileName);
+      fs.writeFileSync(completedModelsFileName, JSON.stringify(content));
+
+      // restart
+      start();
     });
-
-    // save it as json
-    fs.writeFileSync(path.join(savePath, fileName), JSON.stringify(data));
-    console.info("\nData extraction completed", path.join(savePath, fileName));
-
-    // List converted XML's
-    const completedModelsFileName = "completed-models.json";
-    const content = JSON.parse(
-      fs.readFileSync(completedModelsFileName, "utf-8")
-    );
-    content.push(fileName);
-    fs.writeFileSync(completedModelsFileName, JSON.stringify(content));
   });
-});
+}
